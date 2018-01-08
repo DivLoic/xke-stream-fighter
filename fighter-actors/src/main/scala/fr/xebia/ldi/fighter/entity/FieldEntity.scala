8e8ce06e3e594d17bed7c519273112c965546bc1
Loïc <ldivad@isep.fr>
2018-01-08 18:32:15 +0100

    add the pimary entities

---
+++ fighter-actors/src/main/scala/fr/xebia/ldi/fighter/entity/FieldEntity.scala
@@ -0,0 +1,10 @@
+package fr.xebia.ldi.fighter.entity
+
+/**
+  * Created by loicmdivad.
+  */
+object FieldEntity extends Enumeration {
+  type Field = Value
+  val Castle, District, Desert, River, Distillery = Value
+}
+
