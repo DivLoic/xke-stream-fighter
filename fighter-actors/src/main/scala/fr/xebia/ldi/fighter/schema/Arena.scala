1651d4c1b17c94e095598b1b5b781e5cec6b7256
Loïc <ldivad@isep.fr>
2018-01-13 07:22:37 +0100

    add the arena structure

---
+++ fighter-actors/src/main/scala/fr/xebia/ldi/fighter/schema/Arena.scala
@@ -0,0 +1,24 @@
+package fr.xebia.ldi.fighter.schema
+
+import com.sksamuel.avro4s.{AvroSchema, RecordFormat}
+import fr.xebia.ldi.fighter.entity.ArenaEntity.ArenaEntity
+import org.apache.avro.Schema
+
+/**
+  * Created by loicmdivad.
+  */
+case class Arena(id: Int,
+                 name: String,
+                 `type`: String,
+                 terminals: Int,
+                 location: String)
+
+case object Arena {
+
+  def apply(arena: ArenaEntity): Arena = new Arena(arena.id, arena.name, arena.`type`, arena.terminals, arena.location)
+
+  val arenaSchema: Schema = AvroSchema[Arena]
+
+  val arenaFormat: RecordFormat[Arena] = RecordFormat[Arena]
+
+}
