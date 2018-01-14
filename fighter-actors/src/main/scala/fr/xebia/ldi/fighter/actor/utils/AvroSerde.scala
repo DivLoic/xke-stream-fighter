284577ed162ce418f61d4e5f8ed4cd5eac03e644
Loïc <ldivad@isep.fr>
2018-01-14 11:15:54 +0100

    deal with avro serde in a util class

---
+++ fighter-actors/src/main/scala/fr/xebia/ldi/fighter/actor/utils/AvroSerde.scala
@@ -0,0 +1,46 @@
+package fr.xebia.ldi.fighter.actor.utils
+
+import com.typesafe.config.Config
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde
+import org.slf4j.LoggerFactory
+
+import scala.collection.JavaConverters._
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
+
+/**
+  * Created by loicmdivad.
+  */
+object AvroSerde {
+
+  val kafkaClientKey = "akka.kafka.producer.kafka-clients"
+  val registryKey = "schema.registry.url"
+
+  lazy val logger = LoggerFactory.getLogger(getClass)
+
+  def genericSerdeConfigure(conf: Config): GenericAvroSerde = {
+    val clientConf = conf.getConfig(kafkaClientKey)
+    val prop = Map(registryKey -> clientConf.getString(registryKey)).asJava
+    val genericAvroSerde = new GenericAvroSerde()
+    genericAvroSerde.configure(prop, false)
+    genericAvroSerde
+  }
+
+  def retryCallSchemaRegistry(conf: Config, countdown: Int): Try[String] = {
+    val registryUrl = conf.getString(s"$kafkaClientKey.$registryKey")
+
+    Try(scala.io.Source.fromURL(registryUrl).mkString) match {
+      case result: Success[String] =>
+        logger info "Successfully call the Schema Registry."
+        result
+      case result: Failure[String] if countdown <= 0 =>
+        logger error "Fail to call the Schema Registry for the last time."
+        result
+      case result: Failure[String] if countdown > 0 =>
+        logger error "Fail to call the Schema Registry, retry in 15 secs."
+        Thread.sleep(15.second.toMillis)
+        retryCallSchemaRegistry(conf, countdown -1)
+    }
+  }
+
+}
