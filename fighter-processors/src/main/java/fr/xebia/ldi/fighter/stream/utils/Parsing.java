f29f585972446a56f550b9d449caa53f5c75ecc8
Loïc <ldivad@isep.fr>
2018-01-19 23:08:00 +0200

    add utils functions

---
+++ fighter-processors/src/main/java/fr/xebia/ldi/fighter/stream/utils/Parsing.java
@@ -0,0 +1,89 @@
+package fr.xebia.ldi.fighter.stream.utils;
+
+import fr.xebia.ldi.fighter.schema.Victory;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.joda.time.DateTime;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/**
+ * Created by loicmdivad.
+ */
+public class Parsing {
+
+    public static final String[] characters = {"Ken", "Ryu", "Geki", "Chun-Li", "Akuma", "Sakura", "Dhalsim", "Blair"};
+
+    private static final Schema characterConceptKey = SchemaBuilder.builder()
+            .record("CharacterConceptKey")
+            .namespace("fr.xebia.ldi")
+            .fields()
+            .requiredString("character")
+            .requiredString("concept")
+            .endRecord();
+
+
+    private static final Schema aggValueSchema = SchemaBuilder.builder()
+            .record("VictoriesCountValue")
+            .namespace("fr.xebia.ldi")
+            .fields()
+            .requiredString("dt")
+            .requiredString("concept")
+            .requiredString("character")
+            .requiredLong("victories")
+            .endRecord();
+
+    public static Stream<GenericRecord> generateWindowKeys(String concept){
+        return Arrays.stream(characters)
+                .map((String character) -> generateWindowKey(character, concept));
+    }
+
+    private static GenericRecord generateWindowKey(String character, String concept) {
+        GenericRecord record = new GenericData.Record(characterConceptKey);
+        record.put("character", character);
+        record.put("concept", concept);
+        return record;
+    }
+
+    public static GenericRecord groupedDataKey(Victory v) {
+        GenericRecord record = new GenericData.Record(characterConceptKey);
+        record.put("character", v.getCharacter().getName());
+        record.put("concept", v.getArena().getType());
+        return record;
+    }
+
+    public static String extractArenaId(String roundId){
+        String id;
+        try {
+            id = roundId.split("-")[0];
+        } catch(Throwable ex){
+            id = "";
+        }
+        return id;
+    }
+
+    public static KeyValue<GenericRecord, GenericRecord> parseWindowKey(Windowed<GenericRecord> windowKey, Long count) {
+        String startTime = new DateTime(windowKey.window().start()).toString("HH:mm:ss");
+        String concept = windowKey.key().get("concept").toString();
+        String character = windowKey.key().get("character").toString();
+
+        /*String key = String.format("%1$-" + 10 + "s", concept) + "|| " +
+        String.format("%1$-" + 10 + "s", character) + "|| " +
+        String.format("%1$-" + 10 + "s", startTime) + "|| ";*/
+
+        GenericRecord record = new GenericData.Record(aggValueSchema);
+        record.put("character", character);
+        record.put("concept", concept);
+        record.put("victories", count);
+        record.put("dt", startTime);
+
+        return new KeyValue<>(windowKey.key(), record);
+
+    }
+
+}
