e465a84cffd2b0dacc2a10226a10c37f639f424b
Loïc <ldivad@isep.fr>
2018-02-09 22:33:00 +0200

    delete the old brand name

--- fighter-actors/src/main/scala/fr/xebia/ldi/fighter/entity/ArenaEntity.scala
+++ fighter-actors/src/main/scala/fr/xebia/ldi/fighter/entity/ArenaEntity.scala
@@ -11,16 +11,13 @@ case object ArenaEntity {
                                     val name: String,
                                     val `type`: String,
                                     val terminals: Int,
-                                    val location: String){
+                                    val location: String)
 
-  }
-
-  case object `6E-ARR` extends ArenaEntity(0, "COINOP 6", "PRO", 30, "PARIS, 6EM ARRONDISSEMENT")
-  case object `9E-ARR` extends ArenaEntity(1, "COINOP 9", "FAMILY", 21, "PARIS, 9EM ARRONDISSEMENT")
-  case object `15E-ARR` extends ArenaEntity(2, "COINOP 15", "FUN", 15, "PARIS, 15EM ARRONDISSEMENT")
-  case object `16E-ARR` extends ArenaEntity(3, "COINOP 16", "FUN", 18, "PARIS, 16EM ARRONDISSEMENT")
-  case object DAKAR extends ArenaEntity(4, "COINOP DAKAR", "FAMILY", 45,  "SENEGAL")
-  case object NAMUR extends ArenaEntity(5, "COINOP NAMUR", "FUN", 33,  "BELGIUM")
-  case object ZURICH extends ArenaEntity(6, "COINOP ZURICH", "PRO", 52,  "SWITZERLAND")
-
+  case object `6E-ARR` extends ArenaEntity(0, "ARENA 6", "PRO", 30, "PARIS, 6EM ARRONDISSEMENT")
+  case object `9E-ARR` extends ArenaEntity(1, "ARENA 9", "FAMILY", 21, "PARIS, 9EM ARRONDISSEMENT")
+  case object `15E-ARR` extends ArenaEntity(2, "ARENA 15", "FUN", 15, "PARIS, 15EM ARRONDISSEMENT")
+  case object `16E-ARR` extends ArenaEntity(3, "ARENA 16", "FUN", 18, "PARIS, 16EM ARRONDISSEMENT")
+  case object DAKAR extends ArenaEntity(4, "ARENA DAKAR", "FAMILY", 45,  "SENEGAL")
+  case object NAMUR extends ArenaEntity(5, "ARENA NAMUR", "FUN", 33,  "BELGIUM")
+  case object ZURICH extends ArenaEntity(6, "ARENA ZURICH", "PRO", 52,  "SWITZERLAND")
 }
