6cac2ff032486b00f453f4649510289b16f865e5
Loïc <ldivad@isep.fr>
2018-02-28 04:56:06 +0200

    setup the token provider logic

--- fighter-processors/src/main/java/fr/xebia/ldi/fighter/stream/TokkenProvider.java
+++ fighter-processors/src/main/java/fr/xebia/ldi/fighter/stream/TokkenProvider.java
@@ -1,7 +1,65 @@
 package fr.xebia.ldi.fighter.stream;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import fr.xebia.ldi.fighter.schema.Arena;
+import fr.xebia.ldi.fighter.schema.Player;
+import fr.xebia.ldi.fighter.schema.Round;
+import fr.xebia.ldi.fighter.stream.utils.EventTimeExtractor;
+import fr.xebia.ldi.fighter.stream.utils.JobConfig;
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.Consumed;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Map;
+
+import static org.apache.kafka.streams.Topology.AutoOffsetReset.LATEST;
+
 /**
  * Created by loicmdivad.
  */
 public class TokkenProvider {
+
+    public static void main(String[] args){
+
+        Config config = ConfigFactory.load();
+        Map<String, String> props = JobConfig.mapProperties(config);
+
+        GenericAvroSerde avroSerde = new GenericAvroSerde();
+        avroSerde.configure(props, true);
+
+        SpecificAvroSerde<Round> roundSerde = new SpecificAvroSerde<>();
+        roundSerde.configure(props, false);
+
+        SpecificAvroSerde<Arena> arenaSerde = new SpecificAvroSerde<>();
+        arenaSerde.configure(props, false);
+
+        SpecificAvroSerde<Player> playerSerde = new SpecificAvroSerde<>();
+        playerSerde.configure(props, false);
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, Round> rounds = builder
+                .stream("ROUNDS", Consumed.with(Serdes.String(), roundSerde, new EventTimeExtractor(), LATEST));
+
+        rounds
+
+                .filter((arenaId, round) -> true) // Game
+
+                .filter((arenaId, round) -> true) // Combo number
+
+                .filter((arenaId, round) -> true) // Remaining life points
+
+                .transform(null, "")
+
+                .through("")
+
+                .map(null)
+
+                .to("");
+
+    }
 }
